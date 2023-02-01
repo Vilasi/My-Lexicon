994dc6d9439f19b77236582b33ed62096d826ccb
Vilasi <VilasiCoding@gmail.com>
2023-02-01 05:21:47 -0500

    Initial test runs with mock API data

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import * as url from 'url';
 import idmon from 'idmon';
 import methodOverride from 'method-override';
 import axios from 'axios';
+import word from './word.json' assert { type: 'json' };
 
 //Assign absolute paths independent of Environment
 const __filename = url.fileURLToPath(import.meta.url);
@@ -25,8 +26,20 @@ app.use(express.urlencoded({ extended: true })); // for parsing application/x-ww
 app.use(express.json());
 app.use(methodOverride('_method'));
 
+// DATA
+// const word = [];
+
+// INDEX ROUTE
 app.get('/', (req, res) => {
-  res.send('Welcome to My Lexicon!');
+  //   res.send('Welcome to My Lexicon!');
+  const wordObject = word;
+  //   console.log(word);
+
+  res.render('index', { word: wordObject });
+});
+
+app.get('/words', (req, res) => {
+  res.send('I am the response');
 });
 
 app.listen(port, () => {
